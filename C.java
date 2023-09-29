19c6e656a5e492dbd58ec4c308cac2c6ef4432bb
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2023-09-29 18:16:53 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,37 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    // Construtor da classe C
+    public C(String C1, int C2) {
+        this.C1 = C1;
+        this.C2 = C2;
+    }
+
+    // Getters e Setters para C1 e C2
+    public String getC1() {
+        return C1;
+    }
+
+    public void setC1(String C1) {
+        this.C1 = C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC2(int C2) {
+        this.C2 = C2;
+    }
+
+    // Método MC1
+    public void MC1() {
+        System.out.println("Método MC1 da classe C");
+    }
+
+    // Método MC2
+    public void MC2() {
+        System.out.println("Método MC2 da classe C");
+    }
+}
