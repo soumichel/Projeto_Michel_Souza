32389ff36e0902bad7ad3b66c44a957fe82f57f1
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2023-09-29 19:15:51 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,37 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    // Construtor da classe D
+    public D(int D1, float D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    // Getters e Setters para D1 e D2
+    public int getD1() {
+        return D1;
+    }
+
+    public void setD1(int D1) {
+        this.D1 = D1;
+    }
+
+    public float getD2() {
+        return D2;
+    }
+
+    public void setD2(float D2) {
+        this.D2 = D2;
+    }
+
+    // Método MD1
+    public void MD1() {
+        System.out.println("Método MD1 da classe D");
+    }
+
+    // Método MD2
+    public void MD2() {
+        System.out.println("Método MD2 da classe D");
+    }
+}
